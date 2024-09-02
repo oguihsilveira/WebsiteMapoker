5498a05a5f66c8dbbd351a560624924c963ebea2
oguihsilveira <detgui95@gmail.com>
2024-09-02 19:17:40 -0300

    faltou 1 route, mas já fiz(parcelas_a_pagar)

--- routes/parcelas_a_pagarRoutes.py
+++ routes/parcelas_a_pagarRoutes.py
@@ -1,4 +1,4 @@
 from controllers.parcelas_a_pagarController import parcelas_a_pagarController
 
 def parcelas_a_pagar(app):
-    app.route('/mvto_estoque', methods=['POST', 'GET', 'PUT', 'DELETE'])(parcelas_a_pagarController)
+    app.route('/parcelas_a_pagar', methods=['POST', 'GET', 'PUT', 'DELETE'])(parcelas_a_pagarController)
