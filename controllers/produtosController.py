2ab339bb9d3d3505761ac9c4ad05db00c5ab14a6
oguihsilveira <detgui95@gmail.com>
2024-10-02 17:27:08 -0300

    update (02/10)

--- controllers/produtosController.py
+++ controllers/produtosController.py
@@ -3,59 +3,79 @@ from database.db import db
 from models.produtos import Produtos
 
 def produtosController():
-    # Método POST
+    # Método POST - Adicionar um novo produto
     if request.method == 'POST':
         try:
-            data = request.get_json()
-            produtos = Produtos(codigo=data['codigo'], item=data['item'], marca=data['marca'], tipo=data['tipo'], observacoes=data['observacoes'],preco_compra=data['preco_compra'], preco_venda=data['preco_venda'], status=data['status'], quantidade=data['quantidade'],)
-            db.session.add(produtos)
+            data = request.get_json()  # Alterado para receber dados do JSON
+            # Verificar se o código do produto já existe
+            existing_produto = Produtos.query.filter_by(codigo=data['codigo']).first()
+            if existing_produto:
+                return jsonify({'error': 'Este código de produto já existe'}), 400
+
+            # Criar um novo objeto produto com os dados recebidos
+            produto = Produtos(
+                codigo=data['codigo'],
+                item=data['item'],
+                tipo=data['tipo'],
+                preco_atual=data['preco_atual'],
+                preco_antigo=data.get('preco_antigo', None),
+                status=data['status'],
+                quantidade=data['quantidade'],
+                foto=data['foto'],  # O campo foto deve ser tratado adequadamente no frontend
+                observacoes=data['observacoes'],
+                cod_estoque=data['cod_estoque']
+            )
+
+            db.session.add(produto)
             db.session.commit()
             return jsonify({'message': 'Produto inserido com sucesso'}), 200
         except Exception as e:
-            return jsonify({'error': 'Erro ao cadastrar Produto. Erro: {}'.format(str(e))}), 400
-    
-    # Método GET
+            return jsonify({'error': f'Erro ao cadastrar produto. Erro: {str(e)}'}), 400
+
+    # Método GET - Retornar todos os produtos
     elif request.method == 'GET':
         try:
             data = Produtos.query.all()
-            produto = {'produtos': [produtos.to_dict() for produtos in data]}
-            return produto
-
+            produtos = {'produtos': [produto.to_dict() for produto in data]}
+            return jsonify(produtos), 200
         except Exception as e:
-            return 'Não foi possível buscar Produto. Error: {}'.format(str(e)), 405
-        
-    # Método PUT
+            return jsonify({'error': f'Não foi possível buscar produtos. Erro: {str(e)}'}), 405
+
+    # Método PUT - Atualizar um produto existente
     elif request.method == 'PUT':
         try:
             data = request.get_json()
-            put_produtos_id = data['codigo']
-            put_produtos = Produtos.query.get(put_produtos_id)
-            if put_produtos is None:
-                return {'error': 'Produto não encontrado'}, 404
-            put_produtos.item = data.get('item', put_produtos.item)
-            put_produtos.marca = data.get('marca', put_produtos.marca)
-            put_produtos.tipo = data.get('tipo', put_produtos.tipo)
-            put_produtos.observacoes = data.get('observacoes', put_produtos.observacoes)
-            put_produtos.preco_compra = data.get('preco_compra', put_produtos.preco_compra)
-            put_produtos.preco_venda = data.get('preco_venda', put_produtos.preco_venda)
-            put_produtos.status = data.get('status', put_produtos.status)
-            put_produtos.quantidade = data.get('quantidade', put_produtos.quantidade)
-            # print
-            print(put_produtos.item,put_produtos.marca,put_produtos.tipo,put_produtos.observacoes,put_produtos.preco_compra,put_produtos.preco_venda, put_produtos.status, put_produtos.quantidade)
+            put_produto_id = data['codigo']
+            put_produto = Produtos.query.get(put_produto_id)
+            if not put_produto:
+                return jsonify({'error': 'Produto não encontrado'}), 404
+
+            # Atualizar os campos do produto com base nos dados recebidos
+            put_produto.item = data.get('item', put_produto.item)
+            put_produto.tipo = data.get('tipo', put_produto.tipo)
+            put_produto.preco_atual = data.get('preco_atual', put_produto.preco_atual)
+            put_produto.preco_antigo = data.get('preco_antigo', put_produto.preco_antigo)
+            put_produto.status = data.get('status', put_produto.status)
+            put_produto.quantidade = data.get('quantidade', put_produto.quantidade)
+            put_produto.foto = data.get('foto', put_produto.foto)
+            put_produto.observacoes = data.get('observacoes', put_produto.observacoes)
+            put_produto.cod_estoque = data.get('cod_estoque', put_produto.cod_estoque)
+
             db.session.commit()
-            return 'Produtos alterado com sucesso', 200
+            return jsonify({'message': 'Produto atualizado com sucesso'}), 200
         except Exception as e:
-            return {'error': 'Erro ao atualizar Produtos. Erro{}'.format(e)}, 400
-    
-    # Método DELETE
+            return jsonify({'error': f'Erro ao atualizar produto. Erro: {str(e)}'}), 400
+
+    # Método DELETE - Remover um produto existente
     elif request.method == 'DELETE':
         try:
             codigo = request.args.get('codigo')
             delete_produto = Produtos.query.get(codigo)
             if delete_produto is None:
-                return {'error': 'Produto não encontrado'}, 404
+                return jsonify({'error': 'Produto não encontrado'}), 404
+
             db.session.delete(delete_produto)
             db.session.commit()
-            return 'Produto deletado com sucesso', 200
+            return jsonify({'message': 'Produto deletado com sucesso'}), 200
         except Exception as e:
-            return {'error': 'Erro ao deletar Produto. Erro{}'.format(e)}, 400
+            return jsonify({'error': f'Erro ao deletar produto. Erro: {str(e)}'}), 400
--- models/produtos.py
+++ models/produtos.py
@@ -1,36 +1,42 @@
 from database.db import db
 
 class Produtos(db.Model):
+    
     def to_dict(self):
         return {
             'codigo'        : self.codigo,
             'item'          : self.item,
-            'marca'         : self.marca,
             'tipo'          : self.tipo,
-            'observacoes'   : self.observacoes,
-            'preco_compra'  : self.preco_compra,
-            'preco_venda'   : self.preco_venda,
+            'preco_atual'   : self.preco_atual,
+            'preco_antigo'  : self.preco_antigo,
             'status'        : self.status,
-            'quantidade'    : self.quantidade
+            'quantidade'    : self.quantidade,
+            'foto'          : self.foto,
+            'observacoes'   : self.observacoes,
+            'cod_estoque'   : self.cod_estoque
         }
     
+    # Definição das colunas
     codigo          = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    item            = db.Column(db.String(50))
-    marca           = db.Column(db.String(50))
-    tipo            = db.Column(db.String(50))
-    observacoes     = db.Column(db.String(50))
-    preco_compra    = db.Column(db.Float)
-    preco_venda     = db.Column(db.Float)
-    status          = db.Column(db.String(50))
-    quantidade      = db.Column(db.Integer)
+    item            = db.Column(db.String(50), nullable=False)
+    tipo            = db.Column(db.String(50), nullable=False)
+    preco_atual     = db.Column(db.Float(precision=2), nullable=False)
+    preco_antigo    = db.Column(db.Float(precision=2))
+    status          = db.Column(db.String(50), nullable=False)
+    quantidade      = db.Column(db.Integer, nullable=False)
+    foto            = db.Column(db.String(255), nullable=False)
+    observacoes     = db.Column(db.String(50), nullable=False)
+    cod_estoque     = db.Column(db.Integer, db.ForeignKey('estoque.codigo'), nullable=False)
 
-    def __init__(self, codigo, item, marca, tipo, observacoes, preco_compra, preco_venda, status, quantidade):
+    # Construtor
+    def __init__(self, codigo, item, tipo, preco_atual, preco_antigo, status, quantidade, foto, observacoes, cod_estoque):
         self.codigo         = codigo
         self.item           = item
-        self.marca          = marca
         self.tipo           = tipo
-        self.observacoes    = observacoes
-        self.preco_compra   = preco_compra
-        self.preco_venda    = preco_venda
+        self.preco_atual    = preco_atual
+        self.preco_antigo   = preco_antigo
         self.status         = status
         self.quantidade     = quantidade
+        self.foto           = foto
+        self.observacoes    = observacoes
+        self.cod_estoque    = cod_estoque
