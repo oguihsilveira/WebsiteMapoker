3b052a5f97df8c8c0d5d7dc2926fc2993efcb5b2
oguihsilveira <detgui95@gmail.com>
2024-10-09 17:26:31 -0300

    galeria update 09.10

--- src/Components/Galeria/ContentGaleria/ContentGaleria.jsx
+++ src/Components/Galeria/ContentGaleria/ContentGaleria.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./ContentGaleria.css";
 import sala_reunioes from "../../../assets/sala-reunioes.jpg";
 import jardim from "../../../assets/jardim.jpg";
@@ -6,80 +6,114 @@ import ambiente_lazer from "../../../assets/ambiente-lazer.jpg";
 import escritorio from "../../../assets/escritorio.jpg";
 
 const ContentGaleria = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedTitle, setSelectedTitle] = useState("");
+
   useEffect(() => {
-    window.scrollTo(0, 0); // Move o scroll para o topo
+    window.scrollTo(0, 0);
   }, []);
+
+  const openModal = (imageSrc, title) => {
+    setSelectedImage(imageSrc);
+    setSelectedTitle(title);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage("");
+    setSelectedTitle("");
+  };
 
   return (
     <div className="galerias">
+      {modalOpen && (
+        <div className="modal-overlay">
+          <div>
+            <div className="modal-content">
+              <button className="close-button" onClick={closeModal}>
+                <b>X</b>
+              </button>
+              <img src={selectedImage} alt="Imagem Expandida" />
+            </div>
+            <br />
+            <div className="modal-title">{selectedTitle}</div>
+          </div>
+        </div>
+      )}
 
-    {/* 1a linha */}
-        {/* item 1 */}
-      <div className="galeria">
+      <div className="galeria" onClick={() => openModal(sala_reunioes, "Ambiente 1")}>
         <img src={sala_reunioes} alt="" />
         <div className="caption">
+          <h2>Ambiente 1</h2>
           <p>Clique para ver!</p>
         </div>
       </div>
-        {/* item 2 */}
-      <div className="galeria">
+
+      <div className="galeria" onClick={() => openModal(ambiente_lazer, "Ambiente 2")}>
         <img src={ambiente_lazer} alt="" />
         <div className="caption">
-            <p>Clique para ver!</p>
-        </div>
-      </div>
-        {/* item 3 */}
-      <div className="galeria">
-        <img src={jardim} alt="" />
-        <div className="caption">
-            <p>Clique para ver!</p>
-        </div>
-      </div>
-
-    {/* 2a linha */}
-    <div className="galeria">
-        <img src={sala_reunioes} alt="" />
-        <div className="caption">
+          <h2>Ambiente 2</h2>
           <p>Clique para ver!</p>
         </div>
       </div>
-        {/* item 2 */}
-      <div className="galeria">
-        <img src={ambiente_lazer} alt="" />
-        <div className="caption">
-            <p>Clique para ver!</p>
-        </div>
-      </div>
-        {/* item 3 */}
-      <div className="galeria">
+
+      <div className="galeria" onClick={() => openModal(jardim, "Ambiente 3")}>
         <img src={jardim} alt="" />
         <div className="caption">
-            <p>Clique para ver!</p>
-        </div>
-      </div>
-
-    {/* 3a linha */}
-    <div className="galeria">
-        <img src={sala_reunioes} alt="" />
-        <div className="caption">
+          <h2>Ambiente 3</h2>
           <p>Clique para ver!</p>
         </div>
       </div>
-        {/* item 2 */}
-      <div className="galeria">
+
+      <div className="galeria" onClick={() => openModal(sala_reunioes, "Ambiente 4")}>
+        <img src={sala_reunioes} alt="" />
+        <div className="caption">
+          <h2>Ambiente 4</h2>
+          <p>Clique para ver!</p>
+        </div>
+      </div>
+
+      <div className="galeria" onClick={() => openModal(ambiente_lazer, "Ambiente 5")}>
         <img src={ambiente_lazer} alt="" />
         <div className="caption">
-            <p>Clique para ver!</p>
+          <h2>Ambiente 5</h2>
+          <p>Clique para ver!</p>
         </div>
       </div>
-        {/* item 3 */}
-      <div className="galeria">
+
+      <div className="galeria" onClick={() => openModal(jardim, "Ambiente 6")}>
         <img src={jardim} alt="" />
         <div className="caption">
-            <p>Clique para ver!</p>
+          <h2>Ambiente 6</h2>
+          <p>Clique para ver!</p>
         </div>
       </div>
-    {/* Galerias */}
+
+      <div className="galeria" onClick={() => openModal(sala_reunioes, "Ambiente 7")}>
+        <img src={sala_reunioes} alt="" />
+        <div className="caption">
+          <h2>Ambiente 7</h2>
+          <p>Clique para ver!</p>
+        </div>
+      </div>
+
+      <div className="galeria" onClick={() => openModal(ambiente_lazer, "Ambiente 8")}>
+        <img src={ambiente_lazer} alt="" />
+        <div className="caption">
+          <h2>Ambiente 8</h2>
+          <p>Clique para ver!</p>
+        </div>
+      </div>
+
+      <div className="galeria" onClick={() => openModal(jardim, "Ambiente 9")}>
+        <img src={jardim} alt="" />
+        <div className="caption">
+          <h2>Ambiente 9</h2>
+          <p>Clique para ver!</p>
+        </div>
+      </div>
     </div>
   );
 };
